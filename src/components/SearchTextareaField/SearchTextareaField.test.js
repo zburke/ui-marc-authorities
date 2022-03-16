27566b9a82b5f75aa57817e1a780ede4e9b17654
Svitlana-Korol <96055497+Svitlana-Korol@users.noreply.github.com>
2022-03-16 15:18:13 +0200

    UIMARCAUTH-74: Advanced search does not work when editing Advanced Search box (#97)
    * UIMARCAUTH-74:Advanced search does not work when editing Advanced Search box
    
    * UIMARCAUTH-74: added search to onSubmitSearch
    
    * UIMARCAUTH-74 Use rowState from AdvancedSearch
    
    * UIMARCAUTH-74: added test
    
    * UIMARCAUTH-74: fixed code smells
    
    * UIMARCAUTH-74: fixed comments
    
    * UIMARCAUTH-74: fixed comments
    
    * UIMARCAUTH-74: fixed problems with shortcuts
    
    * UIMARCAUTH-74: fixed test errors
    
    * UIMARCAUTH-74: fixed comments
    
    Co-authored-by: Denys Bohdan <Denys_Bohdan@epam.com>

--- src/components/SearchTextareaField/SearchTextareaField.test.js
+++ src/components/SearchTextareaField/SearchTextareaField.test.js
@@ -1,6 +1,5 @@
 import {
   render,
-  fireEvent,
 } from '@testing-library/react';
 
 import SearchTextareaField from './SearchTextareaField';
@@ -56,31 +55,5 @@ describe('Given SearchTextareaField', () => {
     const { getByTestId } = renderSearchTextareaField();
 
     expect(getByTestId('search-textarea')).toBeDefined();
-  });
-
-  describe('when user press "Enter" button', () => {
-    it('should handle form submit', async () => {
-      const { getByTestId } = renderSearchTextareaField();
-
-      fireEvent.change(getByTestId('search-textarea'), { target: { value: 'test' } });
-      fireEvent.keyDown(getByTestId('search-textarea'), {
-        keyCode: 13,
-      });
-
-      expect(mockOnSubmitSearch).toHaveBeenCalled();
-    });
-  });
-
-  describe('when user press not "Enter" button', () => {
-    it('should not handle form submit', async () => {
-      const { getByTestId } = renderSearchTextareaField();
-
-      fireEvent.change(getByTestId('search-textarea'), { target: { value: 'test' } });
-      fireEvent.keyDown(getByTestId('search-textarea'), {
-        keyCode: 14,
-      });
-
-      expect(mockOnSubmitSearch).not.toHaveBeenCalled();
-    });
   });
 });
